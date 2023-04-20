4bacb16b0aa057523a75ad5dfb985354c5274f80
Thanathorn15 <thanathorn_ko@kkumail.com>
2023-04-20 13:13:19 +0700

    Finish new vertion

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,50 +1,53 @@
 const path = require("path");
-const {merge} = require('webpack-merge');
+const { merge } = require("webpack-merge");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const CssMinimizer = require('css-minimizer-webpack-plugin')
-const commonConfig = require('./webpack.config')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizer = require("css-minimizer-webpack-plugin");
+const commonConfig = require("./webpack.config");
 
-const proConfig = merge(commonConfig, {
-    mode : 'production',
+const devConfig = merge(commonConfig, {
+  mode: "production",
 
-    output: {
-        path: path.resolve(__dirname, "docs"),
-        filename: "[name].[hash].min.js",
-        assetModuleFilename: "images/[hash][ext][query]",
+  output: {
+    path: path.resolve(__dirname, "docs"),
+    filename: "[name].[hash].min.js",
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
-      module: {
-        rules: [
-          {
-            test: /\.s?css$/i,
-            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-          },
-        ],
-      },
+    ],
+  },
 
-      plugins: [
-        new MiniCssExtractPlugin(),
-        new CleanWebpackPlugin(),],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/template/index.html",
+    }),
+  ],
 
-      optimization: {
-        minimizer: [
-            new TerserPlugin(),
-            new CssMinimizer(),
-            new HtmlWebpackPlugin({
-                template: './src/template/index.html',
-                filename: 'index.min.html',
-                minify: {
-                    removeAttributeQuotes: true,
-                    collapseWhitespace: true,
-                    removeComments: true,
-                },
-            }),
-        ],
-    },
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizer(),
+      new HtmlWebpackPlugin({
+        template: "./src/template/index.html",
+        filename: "index.min.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+    ],
+  },
 });
 
-module.exports = proConfig;
-    
+module.exports = devConfig;
